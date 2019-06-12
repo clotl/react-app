eeab887af009396810a418e6c6d7a8a89fdc345e
Cloe Truel <cloe.truel@gmail.com>
2019-06-12 12:40:03 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Form from "./Form";
+// import FormSecond from "./FormSecond";
+import Formulaire from "./Form";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
-				<Form />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {/* <FormSecond /> */}
+        <Formulaire />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,47 +1,48 @@
 import React, { Component } from "react";
 
-class FormEmployee extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: "",
-			globalTitle: ""
-		};
-		this.onChange = this.onChange.bind(this);
-		this.submitForm = this.submitForm.bind(this);
-	}
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      globalTitle: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  componentDidMount() {
+    console.log("Formulaire rendu");
+  }
+  componentDidUpdate() {
+    console.log("Titre changé");
+  }
 
-	onChange(e) {
-		this.setState({
-			[e.target.name]: e.target.value
-		});
-	}
-	submitForm(e) {
-		e.preventDefault();
-	}
+  handleChange(event) {
+    this.setState({
+      title: event.target.value
+    });
+  }
 
-	render() {
-		return (
-			<div>
-				<h1> Titre de la page !! </h1>
-				<form onSubmit={this.submitForm}>
-					<div>
-						{/* <label htmlFor="title"></label> */}
-						<input
-							type="text"
-							id="title"
-							name="title"
-							onChange={this.onChange}
-							value={this.state.lastname}
-						/>
-					</div>
-					<div>
-						<input type="submit" value="Valider" />
-					</div>
-				</form>
-			</div>
-		);
-	}
+  handleSubmit(event) {
+    this.setState({
+      globalTitle: "Mon formulaire -" + this.state.title
+    });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> {this.state.globalTitle} </h1>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.title} placeholder="Title" type="text" name="title" />
+          <div>
+            <input type="submit" value="valider" />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default FormEmployee;
+export default Form;
